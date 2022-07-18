d5b3abd9b02037f9d7559db60cbf9fabc2cdabb7
Ravikiran022638 <kiranravi022@gmail.com>
2022-07-18 14:55:22 +0530

    ravi

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb+srv://samirlohiya909:Lohiya123@samirlohiya.nszppy8.mongodb.net/Project-4group04Database?retryWrites=true&w=majority",{
-    useNewUrlsParser:true
+    useNewUrlParser:true
 })
 .then(()=>console.log("MongoDb is connected"))
 .catch(err=>{
