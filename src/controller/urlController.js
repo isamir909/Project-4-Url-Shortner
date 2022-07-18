161b709dd0454a69a38003b63426124bade07dd7
isamir909 <samirlohiya909@gmail.com>
2022-07-18 15:31:16 +0530

    added post url

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,36 +1,25 @@
 const urlModel=require('../model/urlModel')
 const shortId=require('short-unique-id')
 const validUrl = require('valid-url');
-const {isValid}=require('../middleware/validation')
 
 
 
-// <!-- input ww.bnxdbn.com
-// uniqueid
-// http://localhost:3000/+uniqueid
-// res:http://localhost:3000/+uniqueid -->
 
-// - Create a short URL for an original url recieved in the request body.
-// - The baseUrl must be the application's baseUrl. Example if the originalUrl is http://abc.com/user/images/name/2 then the shortened url should be http://localhost:3000/xyz
 const createShortUrl=async function(req,res){
 
     try {
         const data=req.body
+        const {longUrl}=data
 
         if(Object.keys(data).length==0)return res.status(400).send({status:false,msg:"object can not be empty,enter valid data"})
-
+        if((typeof longUrl) != "string")return res.status(400).send({status:false,msg:"url must be string"})
+    
         if(!data.longUrl.trim())return res.status(400).send({status:false,msg:"longUrl is required and can not be empty"})
-        
-     //   if(!isValid(data.longUrl))return res.status(400).send({status:false,msg:"input must be type string"})
-
+       
         if(!validUrl.isWebUri(data.longUrl.trim()))return res.status(400).send({status:false,msg:"enter valid Url"})
  
-
-       
-        //validation for null undefined
-       
-        //string validation
-        //Db call to check uniqueness of long url
+        const findUrl=await urlModel.findOne({longUrl:longUrl})
+        if(findUrl)return res.status(201).send({status:true,msg:"data created sucessfully",data:findUrl})
         
         const uid=new shortId({length: 7})
         const urlCode=uid();
--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -6,14 +6,8 @@
 // }
 
 
-const isValid = function (value) {
-    console.log(typeof value)
-    if (typeof value === 'undefined' || value === null) return false
-    if (typeof value === 'string' && value.trim().length === 0) return false
-    return true;
-}
 
 
 
 
-module.exports=isValid
+module.exports.isValid=isValid
