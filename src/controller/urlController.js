78e5ed36f903b97b0b2042a7fa6c6b39fa63eec4
isamir909 <samirlohiya909@gmail.com>
2022-07-21 11:50:09 +0530

    modified get url

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -3,6 +3,10 @@ const urlModel=require('../model/urlModel')
 const shortId=require('shortid')
 const validUrl = require('valid-url');
 
+
+
+
+
 const createShortUrl=async function(req,res){
 
     try {
@@ -13,11 +17,12 @@ const createShortUrl=async function(req,res){
         if((typeof longUrl) != "string")return res.status(400).send({status:false,msg:"url must be string"})
     
         if(!data.longUrl.trim())return res.status(400).send({status:false,msg:"longUrl is required and can not be empty"})
-       
-        if(!validUrl.isUri(data.longUrl.trim()))return res.status(400).send({status:false,msg:"enter valid Url"})
+        const regex = (/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+
+        if(!regex.test(longUrl)) return  res.status(400).send({status:false,msg:"enter valid Url"})
  
         const findUrl=await urlModel.findOne({longUrl:longUrl}).select({urlCode:1,shortUrl:1,longUrl:1,_id:0})
-        if(findUrl)return res.status(200).send({status:true,msg:"shortened url successfully",data:findUrl})
+        if(findUrl)return res.status(200).send({status:true,data:findUrl})
          
         const urlCode=shortId.generate()
         data['urlCode']=urlCode
@@ -28,7 +33,7 @@ const createShortUrl=async function(req,res){
        const createData=await urlModel.create(data)
        let shortenedUrl ={longUrl:createData.longUrl,shortUrl:createData.shortUrl,urlCode:createData.urlCode}
        
-        return res.status(201).send({status:true,msg:"shortened url successfully",data:shortenedUrl})
+        return res.status(201).send({status:true,data:shortenedUrl})
 
     } catch (error) {
         return res.status(500).send({msg:error.msg})
